662aa170b8518703af93f448a57d4e56e1700eb6
Paul Marley <p.marley1993@gmail.com>
2022-05-12 11:40:54 -0500

    Home update

--- isa-blog/src/pages/index.tsx
+++ isa-blog/src/pages/index.tsx
@@ -1,7 +1,13 @@
-import * as React from "react"
-import { motion } from "framer-motion"
+import React, { useState, useEffect } from "react"
 import { Link, graphql } from "gatsby"
 import Img from "gatsby-image"
+import {
+  motion,
+  useViewportScroll,
+  useSpring,
+  useTransform,
+  AnimatePresence,
+} from "framer-motion"
 import {
   Container,
   Image,
@@ -41,29 +47,93 @@ const BlogIndex = ({
     </GridItem>
   ))
 
+  const [currentPercent, setCurrentPercent] = useState(100)
+  const [currentProgressColor, setCurrentProgressColor] = useState(null)
+  const [progressHeight, setProgressHeight] = useState(70)
+  //Scroll y progress = vertical scroll progres between 0 - 1
+  const { scrollYProgress } = useViewportScroll()
+  const pathLength = useSpring(scrollYProgress, { stiffness: 400, damping: 90 })
+  const yRange = useTransform(scrollYProgress, [0, 1], [0, 100])
+
+  console.log("Current", currentPercent)
+
+  useEffect(
+    () =>
+      yRange.onChange(v => {
+        setCurrentPercent(100 - Math.trunc(yRange.current))
+      }),
+    [yRange]
+  )
+
   return (
-    <Layout location={location}>
-      <Flex
-        flexDir={"column"}
-        // minHeight="90vh"
-        justify="space-between"
-      >
-        <Box mb={5}>
-          <Grid
-            w="100%"
-            templateColumns={[
-              "repeat(1, 1fr)",
-              "repeat(2, 1fr)",
-              "repeat(3, 1fr)",
-              "repeat(4, 1fr)",
-            ]}
-            gap={4}
+    // <Layout location={location}>
+    <>
+      <Box minH={"200vh"} width="100%">
+        <Flex h="100px" justify={"center"} w="100%">
+          <Box width="100%" height="75vh" position="absolute">
+            <motion.div
+              style={{
+                position: "absolute",
+                left: 0,
+                top: 0,
+                width: "100%",
+                height: `${currentPercent}%`,
+                background: "#F8F4F0",
+              }}
+            />
+          </Box>
+          <Flex
+            position="relative"
+            h="100%"
+            justify={"space-between"}
+            align="center"
+            w="75%"
           >
-            {blogCards}
-          </Grid>
-        </Box>
-      </Flex>
-    </Layout>
+            <Heading size="md">Un Cafe con Isa</Heading>
+            <HStack>
+              <Text fontSize={"md"}>Home</Text>
+              <Text fontSize={"md"}>About</Text>
+              <Text fontSize={"md"}>Contact</Text>
+            </HStack>
+            <HStack>
+              <Text fontSize={"md"}>Home</Text>
+              <Text fontSize={"md"}>About</Text>
+              <Text fontSize={"md"}>Contact</Text>
+            </HStack>
+          </Flex>
+        </Flex>
+        <Flex width={"100%"} mt={20} justify="center">
+          <Flex w="75%" position="relative">
+            <Flex w="50%" flexDir={"column"}>
+              <Heading size="md">Un blog de viajes, libros y comida</Heading>
+              <Heading size="2xl" mb={5} mt={3}>
+                I Shouted Above <br /> the Sudden Noise
+              </Heading>
+              <Text w="60%" size="sm">
+                In this article + guide we will cover all the necessary tools
+                and techniques required to create an animation that tracks the
+                progress of a user's position
+              </Text>
+              <Button mt={4} w="30%">
+                Read more
+              </Button>
+            </Flex>
+            <Flex w="50%">
+              <Image
+                w="500px"
+                top={"-15%"}
+                src="https://images.pexels.com/photos/8090376/pexels-photo-8090376.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                alt="Dan Abramov"
+                objectFit="cover"
+                height="500px"
+                position={"absolute"}
+              />
+            </Flex>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+    // </Layout>
   )
 }
 
