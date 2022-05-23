f2bdd8690e7344f6cedb5f63b619ad491fa66977
Paul Marley <p.marley1993@gmail.com>
2022-05-23 18:01:59 -0500

    update

--- isa-blog/src/pages/index.tsx
+++ isa-blog/src/pages/index.tsx
@@ -27,6 +27,7 @@ import Card from "../components/Card"
 import SocialIcons from "../components/SocialIcons"
 import CardButtons from "../components/CardButtons"
 import Footer from "../components/Footer"
+import Navbar from "../components/Navbar/Navbar"
 
 interface BlogIndexProps {
   data: { allContentfulPost: { edges: [{}] } }
@@ -58,84 +59,132 @@ const BlogIndex = ({
   console.log("Current", currentPercent)
 
   const [imagePosition, setImagePosition] = useState(-15)
+  const [divPosition, setDivPosition] = useState(0)
 
+  const [shouldShowActions, setShouldShowActions] = useState(false)
   useEffect(
     () =>
       yRange.onChange(v => {
+        //Can I get pixels?
+        console.log("Vssdf", v)
+        console.log("yRange", yRange.current)
         setCurrentPercent(100 - Math.trunc(yRange.current))
+        setDivPosition(-Math.trunc(yRange.current * 2))
         setImagePosition(imagePosition - Math.trunc(yRange.current * 2))
+        if (Math.trunc(yRange.current) > 15) {
+          setShouldShowActions(true)
+        } else {
+          setShouldShowActions(false)
+        }
+        // if (Math.trunc(yRange.current) < 66.66) {
+        //   setShouldShowActions(false)
+        // } else {
+        //   setShouldShowActions(true)
+        // }
       }),
     [yRange]
   )
+
+  // useEffect(() => {
+  //   function handleScroll() {
+  //     const yPos = window.scrollY
+  //     const isScrollingDown = yPos > 0
+  //     setShouldShowActions(isScrollingDown)
+  //   }
+  //   window.addEventListener("scroll", handleScroll, false)
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll, false)
+  //   }
+  // }, [])
 
   console.log("Image Positon", imagePosition)
 
   return (
     // <Layout location={location}>
     <>
-      <Box minH={"300vh"} width="100%">
-        <Flex h="100px" justify={"center"} w="100%">
-          <Box width="100%" height="75vh" position="absolute">
-            <motion.div
-              style={{
-                position: "absolute",
-                left: 0,
-                top: 0,
-                width: "100%",
-                height: `${currentPercent}%`,
-                background: "#F8F4F0",
-              }}
-            />
-          </Box>
-          <Flex
-            position="relative"
-            h="100%"
-            justify={"space-between"}
-            align="center"
-            w="75%"
-          >
-            <Heading size="md">Un Cafe con Isa</Heading>
-            <HStack>
-              <Button>Home</Button>
-              <Button>About</Button>
-              <Button>Contact</Button>
-            </HStack>
-            <SocialIcons />
-          </Flex>
-        </Flex>
-        <Flex width={"100%"} mt={20} justify="center">
-          <Flex w="75%" position="relative">
-            <Flex w="50%" flexDir={"column"}>
-              <Heading size="md">Un blog de viajes, libros y comida</Heading>
-              <Heading size="2xl" mb={5} mt={3}>
-                I Shouted Above <br /> the Sudden Noise
-              </Heading>
-              <Text w="60%" size="sm">
-                In this article + guide we will cover all the necessary tools
-                and techniques required to create an animation that tracks the
-                progress of a user's position
-              </Text>
-              <Button mt={4} w="30%">
-                Read more
-              </Button>
-            </Flex>
-            <Flex w="50%">
+      <Box
+        minH={"300vh"}
+        maxH={"300vh"}
+        width="100%"
+        // bg={currentPercent < 66.66 && "green"}
+      >
+        <Navbar shouldShowActions={shouldShowActions} />
+        <Box maxHeight="100vh" minHeight="100vh">
+          <Flex h="100px" justify={"center"} w="100%">
+            <Box width="100%" height="75vh" position="absolute">
               <motion.div
-                // style={{ position: "absolute", top: `${imagePosition}%` }}
-                style={{ position: "relative" }}
-              >
-                <Image
-                  w="500px"
-                  src="https://images.pexels.com/photos/8090376/pexels-photo-8090376.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-                  alt="Dan Abramov"
-                  objectFit="cover"
-                  height="300px"
-                  // position={"absolute"}
-                />
-              </motion.div>
+                style={{
+                  position: "absolute",
+                  left: 0,
+                  top: 0,
+                  width: "100%",
+                  height: `${currentPercent}%`,
+                  background: "#F8F4F0",
+                }}
+              />
+            </Box>
+            <Flex
+              // position="relative"
+              h="100%"
+              justify={"space-between"}
+              align="center"
+              w="75%"
+            >
+              <Heading size="md">Un Cafe con Isa</Heading>
+              <HStack>
+                <Button>Home</Button>
+                <Button>About</Button>
+                <Button>Contact</Button>
+              </HStack>
+              <SocialIcons />
             </Flex>
           </Flex>
-        </Flex>
+
+          <Flex width={"100%"} mt={20} justify="center">
+            <Flex w="75%" position="relative">
+              <Flex w="50%" flexDir={"column"}>
+                <motion.div
+                  initial={{ y: 0 }}
+                  animate={{ y: divPosition * 2 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <Heading size="md">
+                    Un blog de viajes, libros y comida
+                  </Heading>
+                  <Heading size="2xl" mb={5} mt={3}>
+                    I Shouted Above <br /> the Sudden Noise
+                  </Heading>
+                  <Text w="60%" size="sm">
+                    In this article + guide we will cover all the necessary
+                    tools and techniques required to create an animation that
+                    tracks the progress of a user's position
+                  </Text>
+                  <Button mt={4} w="30%">
+                    Read more
+                  </Button>
+                </motion.div>
+              </Flex>
+              <Flex w="50%">
+                <motion.div
+                  initial={{ y: 0 }}
+                  animate={{ y: divPosition * 2 }}
+                  transition={{ duration: 0.2 }}
+                  // style={{ position: "absolute", top: `${imagePosition}%` }}
+                  // style={{ position: "relative" }}
+                >
+                  <Image
+                    w="500px"
+                    src="https://images.pexels.com/photos/8090376/pexels-photo-8090376.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                    alt="Dan Abramov"
+                    objectFit="cover"
+                    height="300px"
+                    // position={"absolute"}
+                  />
+                </motion.div>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Box>
         {/* This remains the navBar */}
         <Flex
           mt={"80px"}
@@ -144,15 +193,6 @@ const BlogIndex = ({
           align="center"
           justify="center"
         >
-          <Flex w="75%" justify="space-between">
-            <Heading size="md">Un Cafe con Isa</Heading>
-            <HStack>
-              <Button>Comida</Button>
-              <Button>Viajes</Button>
-              <Button>Libros</Button>
-            </HStack>
-            <SocialIcons />
-          </Flex>
           <Flex mt={20} w="75%">
             <Grid templateColumns="repeat(4, 1fr)" gap={4}>
               {blogCards}
